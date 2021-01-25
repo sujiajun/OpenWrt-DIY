e57a5f3bed91a717c201e69b6f9f0724aa0a8881
sujiajun <dennycai@qq.com>
2021-01-25 17:49:26 +0800

    Update mt7621.mk

--- build/diy/target/linux/ramips/image/mt7621.mk
+++ build/diy/target/linux/ramips/image/mt7621.mk
@@ -1196,10 +1196,25 @@ TARGET_DEVICES += zio_freezio
 
 define Device/zte_e8820s
   IMAGE_SIZE := 32768k
-  IMAGES += factory.bin
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820S
   DEVICE_PACKAGES := \
     kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad luci
+endef
+TARGET_DEVICES += zte_e8820s
+
+define Device/zte_e8820s
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 4096k
+  UBINIZE_OPTS := -E 5
+  IMAGE_SIZE := 32768k
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
+	check-size
+  DEVICE_VENDOR := ZTE
+  DEVICE_MODEL := E8820S
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 wpad-openssl
 endef
 TARGET_DEVICES += zte_e8820s
