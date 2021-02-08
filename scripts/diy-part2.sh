75fcbd67220028abe583c05f324e61880f0ffb16
sujiajun <dennycai@qq.com>
2021-02-08 20:28:01 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -47,6 +47,8 @@ git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
 # svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome package/AdGuardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
